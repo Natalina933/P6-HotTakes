1bfaafc7066d9ff6144378843a5614d0b3513fc7
Nathalie <nat933@free.fr>
2023-02-10 11:29:12 +0100

    Modification createSauce

--- controllers/sauceController.js
+++ controllers/sauceController.js
@@ -4,49 +4,36 @@ const Sauce = require("../models/SauceModel");
 //Création de la Sauce
 exports.createSauce = (req, res, next) => {
   const sauceObjet = json.parse(req.body.sauce);
-  // delete sauce._id;
+  // delete sauceObject._id;
   // delete sauce._userId;
   const sauce = new Sauce({
     ...sauceObjet,
-    userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  });// url de l'image enregistrer dans la bdd et dans le fichier images
-  sauce.save()// la sauce est sauvegardée dans la bdd
-  .then(()=>{
-    res.status(201).json({message: "Sauce enregistrée !"})
-  })
-  .catch(error =>{
-    res.status(400).json({error})
-  });
+    imageUrl: `${req.protocol}://${req.get("host")}/public/images/${
+      req.file.filename
+    }`,
+  }); // url de l'image enregistrer dans la bdd et dans le fichier images
+  sauce
+    .save() // la sauce est sauvegardée dans la bdd
+    .then(() => {
+      res.status(201).json({ message: "Sauce enregistrée !" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 };
-//Nouvel objet sauce
 
-// url de l'image enregistrer dans la bdd et dans le fichier images
-
-// sauce
-//   .save() // la sauce est sauvegardée dans la bdd
-//   .then(() => res.status(201).json({ message: "Sauce sauvegardée" }))
-//   .catch((error) => res.status(400).json({ error }));
-
-//Modifier une sauce
-
-//supprimer une sauce
-
-// Récupération de toutes les sauces
 exports.getAllSauces = (req, res, next) => {
+  // Récupération de toutes les sauces
   Sauce.find()
-  .then((sauces) => res.status(200).json(sauces))
-  .catch((error) => res.status(400).json({ error }));
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+  // Récupération d'une seule sauce
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
 };
 
 
-// // Récupération d'une seule sauce
-// Sauce.findOne({ _id: req.params.id })
-//   .then((sauce) => res.status(200).json(sauce))
-//   .catch((error) => res.status(404).json({ error }));
-
-//Boutons Like et dislike
-// bouton like
-// bouton dislike
-
-// Annuler le like ou le dislike
--- models/sauceModel.js
+++ models/sauceModel.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 const sauceSchema = mongoose.Schema({
   // schema sauce
-    name: { type: String, required: true },  
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true },//le principal ingrédient épicé de la sauce
-    imageUrl: { type: String },//l'URL de l'image de la sauce téléchargée par l'utilisateur
-    heat : { type: Number},
-    likes: { type: Number, default: 0 },
-    dislikes: { type: Number, default: 0 },
-    userId: { type: String },//l'identifiant MongoDB unique de l'utilisateur qui a créé la sauce
-    usersLiked: [{type:String}],
-    usersDisliked: [{type :String}] ,
-
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true }, //le principal ingrédient épicé de la sauce
+  imageUrl: { type: String }, //l'URL de l'image de la sauce téléchargée par l'utilisateur
+  heat: { type: Number },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  userId: { type: String }, //l'identifiant MongoDB unique de l'utilisateur qui a créé la sauce
+  usersLiked: [{ type: String }],
+  usersDisliked: [{ type: String }],
 });
 module.exports = mongoose.model("Sauce", sauceSchema);
