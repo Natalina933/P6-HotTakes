3cb742279bdc0b2010946389502d9287a4a06113
Nathalie <nat933@free.fr>
2023-02-13 19:02:32 +0100

    correction : sauceController commentaires

--- controllers/sauceController.js
+++ controllers/sauceController.js
@@ -30,11 +30,11 @@ exports.getAllSauces = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
-// exports.getOneSauce = (req, res, next) => {
-//   // Récupération d'une seule sauce
-//   Sauce.findOne({ _id: req.params.id })
-//     .then((sauce) => res.status(200).json(sauce))
-//     .catch((error) => res.status(404).json({ error }));
-// };
+exports.getOneSauce = (req, res, next) => {
+  // Récupération d'une seule sauce
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
 
 
